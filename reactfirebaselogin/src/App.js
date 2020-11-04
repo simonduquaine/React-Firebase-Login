2af83e63fb2679fa59d0e797ce262089a0d12fdb
Duquaine Simon <duquaine.simon@gmail.com>
2020-11-04 17:23:06 +0100

    finished function

--- reactfirebaselogin/src/App.js
+++ reactfirebaselogin/src/App.js
@@ -10,7 +10,18 @@ const App = () => {
   const [passwordError, setPasswordError] = useState("");
   const [hasAccount, setHasAccount] = useState(false);
 
+  const clearInputs = () => {
+    setEmail("");
+    setPassword("");
+  };
+
+  const clearErrors = () => {
+    setEmailError("");
+    setPasswordError("");
+  };
+
   const handleLogin = () => {
+    clearErrors();
     fire
       .auth()
       .signInWithEmailAndPassword(email, password)
@@ -27,6 +38,43 @@ const App = () => {
         }
       });
   };
+
+  const handleSignUp = () => {
+    clearErrors();
+    fire
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch((err) => {
+        switch (err.code) {
+          case "auth/email-already-in-use":
+          case "auth/invalid-email":
+            setEmailError(err.message);
+            break;
+          case "auth/weak-password":
+            setPasswordError(err.message);
+            break;
+        }
+      });
+  };
+
+  const handleLogout = () => {
+    fire.auth().signOut();
+  };
+
+  const authListener = () => {
+    fire.auth().onAuthStateChanged((user) => {
+      if (user) {
+        clearInputs();
+        setUser(user);
+      } else {
+        setUser("");
+      }
+    });
+  };
+
+  useEffect(() => {
+    authListener();
+  });
 
   return (
     <div className="App">
